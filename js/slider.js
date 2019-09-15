3fed691dbc1dca4ae83ac4ac2a1b01b2af00b165
unknown <you@example.com>
2019-09-15 18:43:25 +0200

    Finished home page (for now)

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,7 @@
 
 let sliderData = [];
 let sliderOutput = '';
+let modalSliderData = '';
 let sliderCounter = 3;
 let loadCounter = 0;
 let addSliderData = 1;
@@ -13,7 +14,9 @@ function getSliderData() {
       for(addSliderData;addSliderData<4;addSliderData++){
         loadSliderDrinks();
         document.getElementById(`slide${addSliderData}`).innerHTML = sliderOutput;
+        document.getElementById('slider-best').innerHTML += modalSliderData;
         sliderOutput = '';
+        modalSliderData = '';
       }        
     });
 };
@@ -22,12 +25,16 @@ getSliderData();
 function loadSliderDrinks(){
   for(loadCounter;loadCounter<sliderCounter;loadCounter++){
     sliderOutput += `
-      <div class="item col-12 col-sm-12 col-lg-4"">
+      <div class="item col-12 col-sm-12 col-lg-4">
         <img src="${sliderData.drinks[loadCounter].strDrinkThumb}" class="d-block w-100" alt="...">
         <div class="carousel-caption d-md-block">
           <h5>${sliderData.drinks[loadCounter].strDrink}</h5>
           <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#sliderModal${loadCounter}">Read More</button>
-        <div class="modal fade" id="sliderModal${loadCounter}" tabindex="-1" role="dialog" aria-labelledby="sliderModalScrollableTitle${loadCounter}" aria-hidden="true">
+        </div>
+      </div>
+          `;
+    modalSliderData +=`
+      <div class="modal fade" id="sliderModal${loadCounter}" tabindex="-1" role="dialog" aria-labelledby="sliderModalScrollableTitle${loadCounter}" aria-hidden="true">
           <div class="modal-dialog modal-dialog-scrollable" role="document">
             <div class="modal-content">
               <div class="modal-header">
@@ -45,8 +52,6 @@ function loadSliderDrinks(){
             </div>
           </div>
         </div>
-      </div>
-    </div> 
     `
   }
   sliderCounter += 3;
