ca24f79f677441fa2a15bedb5166c1bdc07eaf9b
unknown <you@example.com>
2019-09-19 10:57:18 +0200

    Changed main.js

--- js/main.js
+++ js/main.js
@@ -3,11 +3,38 @@ let mainData = [];
 let mainCounter = 0;
 let maxCount = 100;
 let mainOutput = ``;
+let ingredientsData = [];
+let ingredientsOutput = ``;
 
 document.getElementById("dropdown").addEventListener('change',addMainData);
 
+function loadCheckbox(){
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list`)
+  .then(res => res.json())
+  .then(data => {
+    ingredientsData = data;
+    console.log(ingredientsData);
+    loadIngredients();
+  })
+}
+
+function loadIngredients(){
+  for(let counter=0;counter<ingredientsData.drinks.length;counter++){
+    ingredientsOutput =`
+    <div class="form-check">
+      <input id="ingredient-${counter}" type="checkbox" value="${ingredientsData.drink[counter].strIngredient1}">
+      <label for="ingredient-${counter}">${ingredientsData.drink[counter].strIngredient1}</label>
+    </div>
+    `
+  }
+  document.getElementById("ingredients").innerHTML += ingredientsOutput;
+  ingredientsOutput = ``;
+}
+
+loadCheckbox();
+
 function addMainData(e){
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${e.value}`)
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${e.target.value}`)
     .then(res => res.json())
     .then(data =>{
       mainData = data;
@@ -16,21 +43,17 @@ function addMainData(e){
 }
 
 function loadMainData(){
+  console.log(mainData);
+
   for(mainCounter;mainCounter<maxCount;mainCounter++){
-    mainOutput = `<div class="card mb-3" style="max-width: 540px;">
-    <div class="row no-gutters">
-      <div class="col-md-4">
-        <img src="${mainData.drinks[mainCounter].strDrinkThumb}" class="card-img" alt="...">
+    mainOutput = `
+    <div class="card">
+      <img src="${mainData.drinks[mainCounter].strDrinkThumb}" class="card-img-top" alt="...">
+      <div id="card-body${mainCounter}" class="card-body">
+        <h5 class="card-title">${mainData.drinks[mainCounter].strDrink}</h5>
       </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">${mainData.drinks[mainCounter].strDrink}</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-    </div>`
+    </div>`;
+    document.getElementById("results").innerHTML += mainOutput;
+    mainOutput = ``;
   }
-  document.getElementById("main").innerHTML += mainOutput;
-  mainOutput = ``;
 }
