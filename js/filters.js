36493f6ff7eee8e8cb1a8b88cbe673e72f678ff3
unknown <you@example.com>
2019-09-21 22:43:13 +0200

    Changed filters.js

--- js/filters.js
+++ js/filters.js
@@ -1,25 +1,49 @@
 
 let filtersData = [];
-let ingredients = [];
 let filtersOutput = ``;
 let counter = 0;
 let maxCount = 100;
 let mainOutput = ``;
 let ingredientsData = [];
-let ingredientsOutput = ``;
+let name = document.getElementById("search-box").value;
+let fetchData = function(tabName,id){
+  tabName = document.querySelector(".tab-pane.active");
+  id = document.getElementById(``).target.value;
+}
 
+document.getElementById("filter-search-button"),addEventListener("click",searchByName);
+
+function searchByName(){
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
+  .then(res => res.json())
+  .then(data =>{
+    filtersData = data;
+    for(counter;counter<filtersData.drinks.length;counter++){
+      filtersOutput += `
+      <div class="card">
+        <img src="${filtersData.drinks[counter].strDrinkThumb}" class="card-img-top" alt="...">
+        <div id="card-body${counter}" class="card-body">
+        <h5 class="card-title">${filtersData.drinks[counter].strDrink}</h5>
+        <p class="card-text">${filtersData.drinks[counter].strInstructions}</p>
+        </div>
+      </div>`;
+    }
+    document.getElementById("filter-results").innerHTML += filtersOutput;
+    counter = 0;
+    filtersData = [];
+    filtersOutput = ``;
+  })
+}
 function addCategories(){
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`)
   .then(res => res.json())
   .then(data =>{
     filtersData = data;
-    console.log(filtersData)
-    for(counter;counter<filtersData.drinks.lenght;counter++){
+    for(counter;counter<filtersData.drinks.length;counter++){
       filtersOutput += `<option value="${filtersData.drinks[counter].strCategory}">${filtersData.drinks[counter].strCategory}</option>`;
     }
     document.getElementById("category-search").innerHTML +=`<select name="category" id="dropdown" class="form-control col-10">` + filtersOutput + `</select>`;
     counter = 0;
-    console.log(filtersOutput);
     filtersData = [];
     filtersOutput = ``;
   })
@@ -31,76 +55,17 @@ function addIngredients(){
   .then(res => res.json())
   .then(data =>{
     filtersData = data;
-    console.log(filtersData)
-    for(counter;counter<filtersData.drinks.lenght;counter++){
-      filtersOutput += `<input type="checkbox" id="${filtersData.drinks[counter].strCategory}" value="${filtersData.drinks[counter].strCategory}">
-                <label for="${filtersData.drinks[counter].strCategory}">${filtersData.drinks[counter].strIngredient1}</label>
+    for(counter;counter<filtersData.drinks.length;counter++){
+      filtersOutput += `<div class="form-checkbox">
+                          <input type="checkbox" id="${filtersData.drinks[counter].strCategory}" value="${filtersData.drinks[counter].strCategory}">
+                          <label for="${filtersData.drinks[counter].strCategory}">${filtersData.drinks[counter].strIngredient1}</label>
+                        </div>  
       `;
     }
     document.getElementById("ingredients-search").innerHTML += filtersOutput;
     counter = 0;
-    console.log(filtersOutput)
     filtersData = [];
     filtersOutput = ``;
   })
 }
 addIngredients();
-/*document.getElementById("dropdown").addEventListener('change',addMainData);
-
-function loadCheckbox(){
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list`)
-  .then(res => res.json())
-  .then(data => {
-    ingredientsData = data;
-    console.log(ingredientsData);
-    for(let counter=0;counter<ingredientsData.drinks.length;counter++){
-      ingredientsOutput =`
-      <div class="form-check">
-        <input id="ingredient-${counter}" type="checkbox" value="${ingredientsData.drink[counter].strIngredient1}">
-        <label for="ingredient-${counter}">${ingredientsData.drink[counter].strIngredient1}</label>
-      </div>
-      `
-    }
-    loadIngredients();
-  })
-}
-
-function loadIngredients(){
-  for(let counter=0;counter<ingredientsData.drinks.length;counter++){
-    ingredientsOutput =`
-    <div class="form-check">
-      <input id="ingredient-${counter}" type="checkbox" value="${ingredientsData.drink[counter].strIngredient1}">
-      <label for="ingredient-${counter}">${ingredientsData.drink[counter].strIngredient1}</label>
-    </div>
-    `
-  }
-
-}
-
-loadCheckbox();
-
-function addMainData(e){
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${e.target.value}`)
-    .then(res => res.json())
-    .then(data =>{
-      mainData = data;
-      loadMainData();
-    })
-}
-
-function loadMainData(){
-  console.log(mainData);
-
-  for(mainCounter;mainCounter<maxCount;mainCounter++){
-    mainOutput = `
-    <div class="card">
-      <img src="${mainData.drinks[mainCounter].strDrinkThumb}" class="card-img-top" alt="...">
-      <div id="card-body${mainCounter}" class="card-body">
-        <h5 class="card-title">${mainData.drinks[mainCounter].strDrink}</h5>
-      </div>
-    </div>`;
-    document.getElementById("results").innerHTML += mainOutput;
-    mainOutput = ``;
-  }
-}
-*/
