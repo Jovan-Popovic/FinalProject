cff804a02d9b3faabd86f46bfa92baa4370b0747
unknown <you@example.com>
2019-09-22 22:45:36 +0200

    Changed filters.js

--- js/filters.js
+++ js/filters.js
@@ -5,33 +5,8 @@ let counter = 0;
 let maxCount = 100;
 let mainOutput = ``;
 let ingredientsData = [];
-let name = ``;
-/*
-document.getElementById("filter-search-button").addEventListener("click",searchByName);
+let selectedOrEnteredValue = ``;
 
-function searchByName(){
-  name = document.getElementById("search-box").value;
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
-  .then(res => res.json())
-  .then(data =>{
-    filtersData = data;
-    for(counter;counter<filtersData.drinks.length;counter++){
-      filtersOutput += `
-      <div class="card">
-        <img src="${filtersData.drinks[counter].strDrinkThumb}" class="card-img-top" alt="...">
-        <div id="card-body${counter}" class="card-body">
-        <h5 class="card-title">${filtersData.drinks[counter].strDrink}</h5>
-        <p class="card-text">${filtersData.drinks[counter].strInstructions}</p>
-        </div>
-      </div>`;
-    }
-    document.getElementById("filter-results").innerHTML += filtersOutput;
-    counter = 0;
-    filtersData = [];
-    filtersOutput = ``;
-  })
-}
-*/
 function addCategories(){
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`)
   .then(res => res.json())
@@ -40,7 +15,7 @@ function addCategories(){
     for(counter;counter<filtersData.drinks.length;counter++){
       filtersOutput += `<option id="category-${counter}" value="${filtersData.drinks[counter].strCategory}">${filtersData.drinks[counter].strCategory}</option>`;
     }
-    document.getElementById("category-search").innerHTML +=`<select name="category" id="dropdown" class="form-control col-10">` + filtersOutput + `</select>`;
+    document.getElementById("dropdown").innerHTML += filtersOutput;
     counter = 0;
     filtersData = [];
     filtersOutput = ``;
@@ -54,13 +29,14 @@ function addIngredients(){
   .then(data =>{
     filtersData = data;
     for(counter;counter<filtersData.drinks.length;counter++){
-      filtersOutput += `<div class="form-checkbox">
-                          <input type="checkbox" id="ingredient-${counter}" value="${filtersData.drinks[counter].strCategory}">
-                          <label for="${filtersData.drinks[counter].strCategory}">${filtersData.drinks[counter].strIngredient1}</label>
-                        </div>  
+      filtersOutput += `
+      <div id="ingredient-${counter}" class="form-checkbox">
+        <input type="checkbox" value="${filtersData.drinks[counter].strCategory}">
+        <label for="${filtersData.drinks[counter].strCategory}">${filtersData.drinks[counter].strIngredient1}</label>
+      </div>  
       `;
     }
-    document.getElementById("ingredients-search").innerHTML += filtersOutput;
+    document.getElementById("form-check").innerHTML += filtersOutput;
     counter = 0;
     filtersData = [];
     filtersOutput = ``;
@@ -68,10 +44,9 @@ function addIngredients(){
 }
 addIngredients();
 
-
-function fetchData(tabName,inputValue){
-  //inputValue = document.getElementById(``).value;
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?${tabName}=${inputValue}`)
+function fetchData(srcOrFil,tabName,inputValue){
+  selectedOrEnteredValue = document.getElementById(/*id*/).value
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/${srcOrFil}.php?${tabName}=${inputValue}`)
   .then(res => res.json())
   .then(data => {
     filtersData = data;
@@ -81,19 +56,18 @@ function fetchData(tabName,inputValue){
         <img src="${filtersData.drinks[counter].strDrinkThumb}" class="card-img-top" alt="...">
         <div id="card-body${counter}" class="card-body">
         <h5 class="card-title">${filtersData.drinks[counter].strDrink}</h5>
-        <p class="card-text">Instructions: ${filtersData.drinks[counter].strInstructions}</p>
         </div>
       </div>`;      
     }
-    document.getElementById("filter-search-button").addEventListener("click",function loadFilterResults(){
-      document.getElementById("filter-results").innerHTML += filtersOutput;
-      counter = 0;
-      filtersData = [];
-      filtersOutput = ``;
-    })
+    document.getElementById("filter-results").innerHTML += filtersOutput;
+    counter = 0;
+    filtersData = [];
+    filtersOutput = ``;
+    selectedOrEnteredValue = ``;
   })
 }
 
-document.getElementById("name-search-tab").addEventListener("click",fetchData("s",/*name.value*/))
-document.getElementById("category-search-tab").addEventListener("click",fetchData("c",/*category.value*/))
-document.getElementById("ingredient-search-tab").addEventListener("click",fetchData("i",/*ingredients.value*/))
+document.getElementById("name-search-button").addEventListener("click",fetchData("search","s","search-box"));
+document.getElementById("category-search-button").addEventListener("click",fetchData("filter","c","dropdown"));
+document.getElementById("ingredient-search-button").addEventListener("click",fetchData("filter","i","form-check"));
+
